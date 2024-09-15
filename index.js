c03d90ee305f204aab24046a284100203459fcad
Rachael-njoroge <rachaelnjoroge669@gmail.com>
2024-09-15 21:30:12 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('playButton').addEventListener('click', function() {
-    var video = document.getElementById('video');
-    video.style.display = 'block';
-    video.play();
-    this.style.display = 'none';
-});
